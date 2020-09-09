d589a36cd3a6d32bb0d574cc432acc4f3a2450dc
Léon Montagard <55874306+leon-montagard@users.noreply.github.com>
2020-09-09 11:07:42 +0200

    Delete script.js

--- jour02/job02/script.js
+++
@@ -1,9 +0,0 @@
-function showhide()
-{
-	if(document.getElementById('invisible').style.display == 'none'){
-    	document.getElementById('invisible').style.display = 'block';
-  		}
-  		else {
-    	document.getElementById('invisible').style.display = 'none';
-		}
-}
